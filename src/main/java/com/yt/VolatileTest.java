1404e14220fb5504807935de340714aec1f8d06d
christian yu <christianyu211@gmail.com>
2023-07-27 07:51:32 +0000

    achieve volatile

--- src/main/java/com/yt/VolatileTest.java
+++ src/main/java/com/yt/VolatileTest.java
@@ -1,5 +1,49 @@
 package com.yt;
 
-public class VolatileTest {
+public class VolatileTest implements Runnable{
+    private volatile boolean canceled = false;
 
+    @Override
+
+    public void run() {
+
+        int num = 0;
+
+        try {
+
+            while (!canceled && num <= 1000000) {
+
+                if (num % 10 == 0) {
+
+                    System.out.println(num + "是10的倍数。");
+
+                }
+
+                num++;
+
+                Thread.sleep(1);
+
+            }
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        VolatileTest r = new VolatileTest();
+
+        Thread thread = new Thread(r);
+
+        thread.start();
+
+        Thread.sleep(3000);
+
+        r.canceled = true;
+
+    }
 }
